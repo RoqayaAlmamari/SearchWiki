146e7df128b531d70f981122fa6be1f59831638d
RoqayaAlmamari <roqayaalmamari@gmail.com>
2023-02-27 00:05:12 -0800

    commiting..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //create scanner to input
+        // Create a new Scanner object to read user input from the console
         Scanner scanner = new Scanner(System.in);
 
+        // Prompt the user to enter a search topic
         System.out.print("Enter a search topic: ");
+
+        // Read the user's input and store it in the "searchTopic" variable
         String searchTopic = scanner.nextLine();
-        //create object to call search()
+
+        // Create a new instance of the WikipediaSearch class
         WikipediaSearch wikipediaSearch = new WikipediaSearch();
-        //call search()
+
+        // Call the search() method of the WikipediaSearch class with the "searchTopic" variable as the argument
         wikipediaSearch.search(searchTopic);
     }
 }
--- src/main/java/org/example/WikipediaSearch.java
+++ src/main/java/org/example/WikipediaSearch.java
@@ -10,50 +10,55 @@ import java.io.IOException;
 
 public class WikipediaSearch {
 
+    // Define the URL of the Wikipedia API search endpoint
     private static final String WIKIPEDIA_API_URL =
-            "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="; //use the search url of wikipedia to get the name of topic and other information
+            "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
 
+    // Initialize the OkHttp client and Gson parser
     private final OkHttpClient client = new OkHttpClient();
     private final Gson gson = new Gson();
 
-
-    //add method for search topic and test it in Main Class
+    // Method for searching Wikipedia with a given search term
     public void search(String searchTopic) {
-        String apiUrl = WIKIPEDIA_API_URL + searchTopic; //use the api url to search topic
+        // Build the API URL by appending the search term to the base URL
+        String apiUrl = WIKIPEDIA_API_URL + searchTopic;
 
+        // Create an HTTP GET request with the API URL
         Request request = new Request.Builder()
                 .url(apiUrl)
                 .build();
 
-
         try {
+            // Send the HTTP request and get the response
             Response response = client.newCall(request).execute();
+            // Read the JSON response as a string
             String jsonResponse = response.body().string();
 
+            // Parse the JSON response into a JsonObject
             JsonObject jsonObject = gson.fromJson(jsonResponse, JsonObject.class);
+            // Get the search results from the JsonObject
             JsonArray searchResults = jsonObject.getAsJsonObject("query").getAsJsonArray("search");
 
+            // Iterate over the search results and print the title, URL, and snippets of the first result
             for (int i = 0; i < Math.min(1, searchResults.size()); i++) {
-                //to get result
+                // Get the title and URL of the search result
                 JsonObject result = searchResults.get(i).getAsJsonObject();
                 String title = result.get("title").getAsString();
-
-                String snippet = result.get("snippet").getAsString().replaceAll("<.*?>", ""); // remove HTML tags
-
-
                 String url = "https://en.wikipedia.org/wiki/" + title.replace(" ", "_");
 
+                // Get the snippet of the search result and remove HTML tags
+                String snippet = result.get("snippet").getAsString().replaceAll("<.*?>", "");
 
+                // Print the title, URL, and snippets of the search result
                 System.out.println("Title:    " + title);
                 System.out.println("URL:      " + url);
-
-                //to get the snippets from different line in wikipedia site
                 System.out.println("Snippet 1:    " + snippet.substring(0, Math.min(snippet.length(), 30)) + "...");
                 System.out.println("Snippet 2:    " + snippet.substring(31, Math.min(snippet.length(), 60)) + "...");
                 System.out.println("Snippet 3:    " + snippet.substring(61, Math.min(snippet.length(), 100)) + "...");
                 System.out.println();
             }
         } catch (IOException e) {
+            // Handle any IO errors
             e.printStackTrace();
         }
     }
